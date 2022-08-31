4f3cbb285718d0605204bb14fc9e9b86f00815b8
David Langevin <dlangevi@uwaterloo.ca>
2022-08-31 12:06:02 -0700

    replace bookcard with naive one

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -1,38 +1,19 @@
 <template>
-  <main>
-    <div class="rounded border m-4
-      bg-white border-3 mb-4 border border-grey" @click="bookBigMode">
-      <img
+  <n-card :title="book.title" class="p-4">
+    <template #cover>
+      <img @click="bookBigMode"
         class="w-full rounded rounded-t"
         :src="'data:image/png;base64,' + book.imgData"
-        alt="Card image cap"
+        :alt="book.title"
       />
-      <div class="p-6">
-        <p class="mb-0">{{ book.title }}</p>
-        <p class="card-text">{{ book.author }}</p>
-        <div class="">
-          <div class="flex justify-content-between align-items-center">
-            <div class="relative inline-flex align-middle">
-              <button type="button" class="inline-block align-middle
-                text-center select-none border font-normal whitespace-no-wrap
-                py-2 px-4 rounded text-base leading-normal no-underline py-1 px-2
-                text-sm leading-tight text-grey-dark border-grey bg-white
-                hover:bg-grey-light hover:text-grey-darker">
-                <i class="fas fa-heart"></i>
-              </button>
-              <button type="button" class="btn btn-sm btn-outline-secondary">
-                <i class="fas fa-plus"></i>
-              </button>
-            </div>
-            <small class="ms-auto text-grey">
-              ID {{book.bookID}}
-            </small>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+    </template>
+    <p>{{ book.author }}</p>
+    <small>
+      ID {{book.bookID}}
+    </small>
+  </n-card>
 </template>
+
 <script setup>
 import { useRouter } from 'vue-router';
 
--- src/pages/BookLibrary.vue
+++ src/pages/BookLibrary.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="container mx-auto px-4">
-    <div class="text-center">
-      <h2 class="text-center text-xl mt-5">Your Library</h2>
-      <p>Click on a book to start making flashcards.</p>
-    </div>
-
-    <div class="grid grid-cols-3" v-if="books.length > 0">
-        <book-card :book="book" v-for="(book, i) in books" :key="i" />
-    </div>
+  <div class="text-center">
+    <h2 class="text-center text-xl mt-5">Your Library</h2>
+    <p>Click on a book to start making flashcards.</p>
   </div>
+
+  <n-grid x-gap="12" y-gap="12" :cols="4" v-if="books.length > 0">
+    <n-gi v-for="(book, i) in books" :key="i">
+      <book-card :book="book" />
+    </n-gi>
+  </n-grid>
 </template>
 
 <script setup>
