5bc4145d46a649cee55de72f471079c15f271273
David Langevin <dlangevi@uwaterloo.ca>
2022-11-22 14:46:09 -0800

    Remove naive-ui from secret text box

--- frontend/src/components/SettingsWidgets/SettingsTextbox.vue
+++ frontend/src/components/SettingsWidgets/SettingsTextbox.vue
@@ -1,18 +1,16 @@
 <template>
-  <n-space class="col-span-2">
+  <div class="col-span-2">
     <span>
       {{ setting.label }}
     </span>
-    <n-input
+    <input
+      v-model="currentValue"
       :type="type"
-      show-password-on="mousedown"
-      class="min-w-8"
+      class="input w-1/2"
       :placeholder="setting.label"
-      :default-value="initialValue"
       :maxlength="32"
       :readonly="readonly"
-      @update:value="updateValue"
-    />
+    >
     <button
       v-if="readonly"
       class="btn-primary btn-sm btn"
@@ -34,15 +32,12 @@
     >
       <information-circle class="h-6 w-6" />
     </div>
-  </n-space>
+  </div>
 </template>
 
 <script lang="ts" setup>
 // TODO have some kind of hover popup with more info
 import { ref } from 'vue';
-import {
-  NInput, NSpace,
-} from 'naive-ui';
 import { InformationCircle } from '@vicons/ionicons5';
 
 const type = ref<'password' | 'text'>('password');
@@ -57,9 +52,6 @@ const props = defineProps({
 const initialValue = await props.setting.read();
 const currentValue = ref(initialValue);
 
-function updateValue(newValue: string) {
-  currentValue.value = newValue;
-}
 function startEdit() {
   readonly.value = false;
   type.value = 'text';
