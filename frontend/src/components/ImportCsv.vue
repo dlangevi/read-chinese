f1505367652333a15e06fb72c5d2020e8d00c683
David Langevin <dlangevi@uwaterloo.ca>
2022-12-13 10:39:07 -0800

    Teleport modal to body

--- frontend/src/components/ImportCsv.vue
+++ frontend/src/components/ImportCsv.vue
@@ -1,25 +1,28 @@
 <template>
   <div>
-    <div
-      :class="['modal', {'modal-open': importCsvModal}]"
-      @click="() => importCsvModal = false"
-    >
-      <div
-        class="modal-box w-1/2 max-w-5xl"
-        @click.stop
-      >
-        Hello
-        <button class="btn-secondary btn" @click="importCsv">
-          Import
-        </button>
-      </div>
-    </div>
     <button
       class="btn-primary btn w-full"
       @click="() => importCsvModal = true"
     >
       Import from CSV
     </button>
+
+    <Teleport to="body">
+      <div
+        :class="['modal', {'modal-open': importCsvModal}]"
+        @click="() => importCsvModal = false"
+      >
+        <div
+          class="modal-box w-1/2 max-w-5xl"
+          @click.stop
+        >
+          Hello
+          <button class="btn-secondary btn" @click="importCsv">
+            Import
+          </button>
+        </div>
+      </div>
+    </Teleport>
   </div>
 </template>
 
