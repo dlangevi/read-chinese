a94532ed58fffa15ad069e9f1afeffba0a2a409e
David Langevin <dlangevi@uwaterloo.ca>
2023-03-08 16:45:25 -0800

    Get the types working for chart

--- frontend/src/views/UserStats.vue
+++ frontend/src/views/UserStats.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex">
+  <div ref="styleRef" class="flex">
     <div class="w-1/5">
       <div class="flex flex-col p-4">
         <div
@@ -26,7 +26,7 @@
       </div>
     </div>
     <div class="h-full w-4/5 p-4">
-      <Line class="" :data="data" :options="options" />
+      <Line v-if="loaded" :data="data" :options="options" />
     </div>
   </div>
 </template>
@@ -44,7 +44,10 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
+import type { ChartData } from 'chart.js';
 import { Line } from 'vue-chartjs';
+// import type { ChartProps } from 'vue-chartjs';
+import { ref, onMounted } from 'vue';
 
 ChartJS.register(
   CategoryScale,
@@ -56,28 +59,50 @@ ChartJS.register(
   Legend,
 );
 
-const totalWords = await TotalRead();
-const totalChars = await TotalReadChars();
-const {
-  words,
-  characters,
-} = await GetWordStats();
+const styleRef = ref<HTMLElement | null>(null);
 
-const rawdata = await GetStatsInfo();
-const data = {
-  labels: rawdata.map(d => d.day),
-  datasets: [{
-    label: 'Known Words',
-    backgroundColor: '#f87979',
-    data: rawdata.map(d => d.known),
-  },
-  {
-    label: 'Known Chars',
-    backgroundColor: '#002200',
-    data: rawdata.map(d => d.knownCharacters),
-  },
-  ],
-};
+const totalWords = ref(0);
+const totalChars = ref(0);
+const words = ref(0);
+const characters = ref(0);
+const data = ref<ChartData<'line'>>({
+  datasets: [],
+});
+const loaded = ref(false);
+
+onMounted(async () => {
+  if (!styleRef.value) {
+    return;
+  }
+  totalWords.value = await TotalRead();
+  totalChars.value = await TotalReadChars();
+
+  const style = getComputedStyle(styleRef.value);
+  const primary = `hsl(${style.getPropertyValue('--p')}`;
+  const secondary = `hsl(${style.getPropertyValue('--s')}`;
+
+  const wordStats = await GetWordStats();
+  words.value = wordStats.words;
+  characters.value = wordStats.characters;
+
+  const rawdata = await GetStatsInfo();
+  data.value = {
+    labels: rawdata.map(d => d.day),
+    datasets: [{
+      label: 'Known Words',
+      backgroundColor: primary,
+      data: rawdata.map(d => d.known),
+    },
+    {
+      label: 'Known Chars',
+      backgroundColor: secondary,
+      data: rawdata.map(d => d.knownCharacters),
+    },
+    ],
+  };
+  loaded.value = true;
+});
+
 const options = {
   responsive: true,
 };
